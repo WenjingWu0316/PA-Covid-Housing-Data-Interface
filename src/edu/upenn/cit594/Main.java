040db0dbdc196dddbd01f206129628bf808fd509
Shuo Li <https://github.com/CIS380/21su-cit595-shuo0314penn.git>
2021-08-14 11:17:18 -0400

    (Li)Added closeLog()

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -5,9 +5,6 @@ import java.io.File;
 
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import edu.upenn.cit594.datamanagement.CSVFileReaderCovid;
 import edu.upenn.cit594.datamanagement.CSVFileReaderProperty;
 import edu.upenn.cit594.datamanagement.JSONFileReaderCovid;
@@ -46,26 +43,28 @@ public class Main {
 		//check whether COVID file is valid
 		if(covidFileName.substring(covidFileName.length()-4).equals(".csv") ) {
 			File file = new File(covidFileName);
-			if(!file.exists() || !file.canRead()) {return;}
+			if(!file.exists() || !file.canRead()) {logging.closeLog();return;}
 			readerForCovid = new CSVFileReaderCovid(covidFileName);
 		}else if(covidFileName.substring(covidFileName.length()-5).equals(".json")) {
 			File file = new File(covidFileName);
-			if(!file.exists() || !file.canRead()) {return;}
+			if(!file.exists() || !file.canRead()) {logging.closeLog();return;}
 			readerForCovid = new JSONFileReaderCovid(covidFileName);
 		}else {
 			System.out.println("ERROR: covid file does not match a recognized extension");
 			System.out.flush();
+			logging.closeLog();
 			return;
 		}
 		
 		//check whether population file is valid
 		if(populationFileName.substring(populationFileName.length()-4).equals(".txt")) {
 			File file = new File(populationFileName);
-			if(!file.exists() || !file.canRead()) {return;}
+			if(!file.exists() || !file.canRead()) {logging.closeLog();return;}
 			readerForPopulation = new PopulationFileReader(populationFileName);
 		}else{
 			System.out.println("ERROR: population file does not match a recognized extension");
 			System.out.flush();
+			logging.closeLog();
 			return;
 		}
 		
@@ -74,13 +73,14 @@ public class Main {
 		if(!file.exists()) {//if file doesn't exist, create new file
 			try {
 				if (!file.createNewFile()) {//if can't create new file, return and terminate the program
+					logging.closeLog();
 					return;
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}else {//if file does exist but can't be written, return and terminate the program
-			if(!file.canWrite()) {return;}
+			if(!file.canWrite()) {logging.closeLog();return;}
 		}
 		
 		
